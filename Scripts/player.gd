93988ec8cabcb062db79c4cf2ab8be40f825d5cf
MinhNguyen <minhnguyen21@augustana.edu>
2024-01-18 14:57:06 -0600

    Rolling and changing collision shape for rolling

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,19 +4,26 @@ extends CharacterBody2D
 @export var jump_force = 500
 
 @onready var player_sprite = $AnimatedSprite2D
+@onready var runningmotion = $Running
+@onready var rollingmotion = $Rolling
 
 func _physics_process(delta):
 	if is_on_floor() == false:
 		velocity.y += gravity * delta
 	if is_on_floor() && (Input.is_action_just_pressed("jump")):
-			jump(jump_force)
+		jump(jump_force)
+	if is_on_floor() && (Input.is_action_pressed("rolling")):
+		rolling()
 	
 	move_and_slide()
 	update_animations()
 	
 func update_animations():
 	if is_on_floor():
-		player_sprite.play("running")
+		if Input.is_action_pressed("rolling"):
+			player_sprite.play("rolling")
+		else:
+			player_sprite.play("running")
 	else:
 		if velocity.y  < 0:
 			player_sprite.play("jump")
@@ -25,5 +32,9 @@ func update_animations():
 			
 func jump(force):
 	velocity.y = -force
+	
+func rolling():
+	runningmotion.disabled = true
+	rollingmotion.disabled = false
 func _ready():
 	pass
