30cfbe45002a9b1051289ae2172df07afe3c4ae2
MinhNguyen <minhnguyen21@augustana.edu>
2024-01-18 15:42:12 -0600

    Adding a raycast

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,22 +3,27 @@ extends CharacterBody2D
 @export var gravity = 1000
 @export var jump_force = 500
 
+@onready var ray = $RayCast2D
 @onready var player_sprite = $AnimatedSprite2D
 @onready var runningmotion = $Running
 @onready var rollingmotion = $Rolling
+@onready var player_camera = $Camera2D
 
 func _physics_process(delta):
 	if is_on_floor() == false:
 		velocity.y += gravity * delta
 	if is_on_floor() && (Input.is_action_just_pressed("jump")):
 		jump(jump_force)
-	if is_on_floor() && (Input.is_action_pressed("rolling")):
+	if is_on_floor() && (Input.is_action_just_pressed("rolling")):
 		rolling()
 	
 	move_and_slide()
 	update_animations()
 	
 func update_animations():
+	if (ray.is_colliding()):
+		
+	print(ray.is_colliding())
 	if is_on_floor():
 		if Input.is_action_pressed("rolling"):
 			player_sprite.play("rolling")
