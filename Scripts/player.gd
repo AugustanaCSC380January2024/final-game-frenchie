f6dac6eff3af71c86f8f0338d22f80765f928f5f
MinhNguyen <minhnguyen21@augustana.edu>
2024-01-18 13:45:19 -0600

    Endless running and jumping

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,5 +1,17 @@
 extends CharacterBody2D
 
+@export var gravity = 1000
+@export var jump_force = 500
 
+@onready var player_sprite = $AnimatedSprite2D
+
+func _physics_process(delta):
+	if is_on_floor() == false:
+		velocity.y += gravity * delta
+	if is_on_floor():	
+		if Input.is_action_pressed("jump"):
+			velocity.y = -jump_force
+	
+	move_and_slide()
 func _ready():
-	add_child(Game.player_sprite)
+	pass
